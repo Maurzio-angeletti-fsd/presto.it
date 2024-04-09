8b9acb297062aba80858c40e571d668cf6d8139e
Maurizio Angeletti <91mangele@gmail>
2024-04-09 10:31:37 +0200

    tolto job google ai

--- app/Livewire/InsertAnnouncement.php
+++ app/Livewire/InsertAnnouncement.php
@@ -107,17 +107,20 @@ class InsertAnnouncement extends Component
                 //     'announcement_id'=> $this->announcement_id,
                 //     'path'=>$image->store('images', 'public')]);
                 $newFileName = "announcement/{$this->announcement_id}";
+
                 $newImage = Image::create([
                     'announcement_id' => $this->announcement_id, 'path' => $image->store($newFileName, 'public')
                 ]);
+                
+                dispatch(new ResizeImage($newImage->path, 300, 200));
+                
+                // RemoveFaces::withChain([
 
-                RemoveFaces::withChain([
+                //     new ResizeImage($newImage->path, 300, 200),
+                //     new GoogleVisionSafeSearch($newImage->id),
+                //     new GoogleVisionLabelImage($newImage->id)
 
-                    new ResizeImage($newImage->path, 300, 200),
-                    new GoogleVisionSafeSearch($newImage->id),
-                    new GoogleVisionLabelImage($newImage->id)
-
-                ])->dispatch($newImage->id);
+                // ])->dispatch($newImage->id);
             }
 
             // File::deleteDirectory(storage_path('app/livewire-tmp'));
